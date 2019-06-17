d08749fea9e4784ce0a7e2542f48e858983dc206
christopherknightmerritt <chris.merritt95@gmail.com>
2019-06-17 13:10:56 -0700

    added basic get all, get one, post, put, delete routes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ const app = express();
 
 const errorHandler = require(`./middleware/500.js`)
 const notFound = require(`./middleware/404.js`);
+const v1Router = require(`./api/v1.js`);
 
+app.use(v1Router);
 app.use(notFound);
 
 
