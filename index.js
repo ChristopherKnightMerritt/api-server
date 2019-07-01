c40986252983b501a7d26bc11538095681fdec84
christopherknightmerritt <chris.merritt95@gmail.com>
2019-07-01 14:55:46 -0700

    update routes

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 
 require('dotenv').config();
 
+const mongoose = require('mongoose');
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+};
 
+mongoose.connect(process.env.MONGODB_URI, options);
 require('./src/app.js').start(process.env.PORT);
