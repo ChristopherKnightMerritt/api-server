ba30293e51e2fa131f7d6b95d950a4553b108145
christopherknightmerritt <chris.merritt95@gmail.com>
2019-07-11 13:55:38 -0700

    set up users model

---
+++ src/auth/users-model.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+
+const users = new mongoose.Schema({
+  username: {type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  email: {type: String },
+  role: { type: String, default: 'user'},
+}, {
+  toObject: {virtuals: true};
+  toJSON: {virtuals: true},
+});
+
+users.virtual('abilities', {
+  ref: 'roles',
+  localfield: 'role',
+  foreignFeild: 'role',
+  justOne: false,
+});
+
