71e6bc06935d87e0956c41016d31cdc781895273
christopherknightmerritt <chris.merritt95@gmail.com>
2019-07-11 13:59:05 -0700

    set up users model

--- src/auth/users-model.js
+++ src/auth/users-model.js
@@ -7,7 +7,7 @@ const users = new mongoose.Schema({
   username: {type: String, required: true, unique: true },
   password: { type: String, required: true },
   email: {type: String },
-  role: { type: String, default: 'user'},
+  role: { type: String, default: 'user', enum: ['superuser', 'admin', 'editor', 'user'] },
 }, {
   toObject: {virtuals: true};
   toJSON: {virtuals: true},
